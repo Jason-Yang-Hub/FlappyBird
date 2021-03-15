b89c85060e5c481f8aaba555df3c7ffeefc370e9
yws <455419563@qq.com>
2021-03-15 21:28:44 +0800

    111

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    Rigidbody2D rg;
+    Rigidbody2D player;
 
     public float upForce = 200f;    
 
@@ -12,15 +12,15 @@ public class Bird : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rg = GetComponent<Rigidbody2D>();
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Controller.isOpen && Input.GetKeyDown("space"))
         {
-            rg.AddForce(new Vector2(0, upForce));
+            player.AddForce(new Vector2(0, upForce));
         }
     }
 }
--- Assets/Colum.cs
+++ Assets/Colum.cs
@@ -1,29 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Colum : MonoBehaviour
 {
     public float speed = 2.0f;
 
+    private float liveTime = 15.0f;
+    private float createTime = 0f;
+    private Text score;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        createTime = Time.time;
+        score = GameObject.Find("TextScore").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (Controller.isOpen) 
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+            if (Time.time - createTime >= liveTime)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //Debug.Log("+1");
+            Controller.score += 1;
+            score.text = "Score:" + Controller.score;
         }
     }
 
